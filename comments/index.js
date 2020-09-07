9f3c3e8e944871b167c96c3b9eb3ae693beba283
Ali Ezadkhaha <hodhod212@yahoo.com>
2020-09-07 16:39:20 +0200

    Created post and comment

---
+++ comments/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import bodyParser from "body-parser";
+import xyz from "crypto";
+import cors from "cors";
+import axios from "axios";
+const { randomBytes } = xyz;
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const commentsByPostId = {};
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+app.post("/posts/:id/comments", async (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content, status: "pending" });
+  commentsByPostId[req.params.id] = comments;
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+      status: "pending",
+    },
+  });
+  res.status(201).send(comments);
+});
+app.post("/events", async (req, res) => {
+  console.log("Received events", req.body.type);
+  const { type, data } = req.body;
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: { id, status, postId, content },
+    });
+  }
+  res.send({});
+});
+app.listen(4001, () => {
+  console.log("Listening on port 4001");
+});
